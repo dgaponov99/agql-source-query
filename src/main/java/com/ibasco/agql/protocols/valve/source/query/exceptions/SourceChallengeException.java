6f359e400440fe46cd826b9c86e48cb058a876f0
Rafael Luis Ibasco <ribasco@gmail.com>
2021-11-08 09:36:12 +0800

    Cleanup code

--- src/main/java/com/ibasco/agql/protocols/valve/source/query/exceptions/SourceChallengeException.java
+++ src/main/java/com/ibasco/agql/protocols/valve/source/query/exceptions/SourceChallengeException.java
@@ -1,18 +1,14 @@
 /*
  * MIT License
- *
- * Copyright (c) 2018 Asynchronous Game Query Library
- *
+ * Copyright (c) 2021 Asynchronous Game Query Library
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,28 +18,25 @@
  * SOFTWARE.
  */
 
-package com.ibasco.agql.protocols.valve.source.query.request;
+package com.ibasco.agql.protocols.valve.source.query.exceptions;
 
-import com.ibasco.agql.protocols.valve.source.query.SourceServerRequest;
-import com.ibasco.agql.protocols.valve.source.query.enums.SourceChallengeType;
-import com.ibasco.agql.protocols.valve.source.query.packets.request.SourceChallengeRequestPacket;
-
-import java.net.InetSocketAddress;
+import com.ibasco.agql.core.exceptions.AsyncGameLibUncheckedException;
 
 /**
- * Created by raffy on 9/15/2016.
+ * Indicates that the server responded with a challenge number. Use the provided challenge number in this instance for your next info query.
+ *
+ * @author Rafael Luis Ibasco
  */
-public class SourceChallengeRequest extends SourceServerRequest<SourceChallengeRequestPacket> {
+public class SourceChallengeException extends AsyncGameLibUncheckedException {
 
-    private final SourceChallengeType type;
+    private final Integer challengeNumber;
 
-    public SourceChallengeRequest(SourceChallengeType type, InetSocketAddress recipient) {
-        super(recipient);
-        this.type = type;
+    public SourceChallengeException(Integer challengeNumber) {
+        super(String.format("Server requires a new challenge number: %d", challengeNumber));
+        this.challengeNumber = challengeNumber;
     }
 
-    @Override
-    public SourceChallengeRequestPacket getMessage() {
-        return new SourceChallengeRequestPacket(type);
+    public Integer getChallengeNumber() {
+        return challengeNumber;
     }
 }
